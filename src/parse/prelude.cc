4e03a2eb6ef63fee8b90f1c90c6962e3afc70650
kate20031 <83357098+kate20031@users.noreply.github.com>
2026-04-28 21:33:19 +0000

    Merge TC-1/2 base code

---
+++ src/parse/prelude.cc
@@ -0,0 +1,24 @@
+#include "parse/tiger-driver.hh"
+
+namespace parse
+{
+  const char* TigerDriver::prelude () const
+  {
+    return
+R"(primitive print(string : string)
+primitive print_err(string : string)
+primitive print_int(int : int)
+primitive flush()
+primitive getchar() : string
+primitive ord(string : string) : int
+primitive chr(code : int) : string
+primitive size(string : string) : int
+primitive streq(s1 : string, s2 : string) : int
+primitive strcmp(s1 : string, s2 : string) : int
+primitive substring(string : string, start : int, length : int) : string
+primitive concat(fst : string, snd : string) : string
+primitive not(boolean : int) : int
+primitive exit(status : int)
+)";
+  }
+} // namespace parse
