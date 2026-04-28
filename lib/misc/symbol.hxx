165c3caa4948b54760ad57c8b8e9909a07448083
kate20031 <83357098+kate20031@users.noreply.github.com>
2026-04-28 22:26:21 +0000

    TC-1/2: implement symbol operators

--- lib/misc/symbol.hxx
+++ lib/misc/symbol.hxx
@@ -11,22 +11,23 @@ namespace misc
 {
   inline symbol& symbol::operator=(const symbol& rhs)
   {
-    // FIXME: Some code was deleted here.
+    super_type::operator=(rhs);
+    return *this;
   }
 
   inline bool symbol::operator==(const symbol& rhs) const
   {
-    // FIXME: Some code was deleted here.
+    return this->get() == rhs.get();
   }
 
   inline bool symbol::operator!=(const symbol& rhs) const
   {
-    // FIXME: Some code was deleted here.
+    return !(*this == rhs);
   }
 
   inline std::ostream& operator<<(std::ostream& ostr, const symbol& the)
   {
-    // FIXME: Some code was deleted here.
+    return ostr << the.get();
   }
 
 } // namespace misc
