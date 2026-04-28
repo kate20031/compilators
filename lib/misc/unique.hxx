a154796ef5660c08447b7683f966478ca12ae1f3
kate20031 <83357098+kate20031@users.noreply.github.com>
2026-04-28 22:21:13 +0000

    TC-1/2: implement unique (flyweight pattern)

--- lib/misc/unique.hxx
+++ lib/misc/unique.hxx
@@ -15,31 +15,47 @@ namespace misc
   /** \brief Following the Flyweight design pattern, set the attribute to a
        unique reference of value s. You might want to check out std::set methods
        on cppreference.com. */
-  {}
+  {
+    auto& objects = object_set_instance();
+    auto res = objects.insert(s);
+    obj_ = &*res.first;
+  }
 
   template <typename T, class C>
   typename unique<T, C>::object_set_type& unique<T, C>::object_set_instance()
   {
     // FIXME: Some code was deleted here (Classical Singleton pattern, a la Scott Meyers').
     /** \brief Create a persistent instance of a set which would hold each value. */
+    
+    static object_set_type objects;
+    return objects;
+
   }
 
   template <typename T, class C>
   typename unique<T, C>::object_size_type unique<T, C>::object_map_size()
   {
     // FIXME: Some code was deleted here.
+    
+    return object_set_instance().size();
+
   }
 
   template <typename T, class C>
   inline const typename unique<T, C>::data_type& unique<T, C>::get() const
   {
     // FIXME: Some code was deleted here.
+    assertion(obj_);
+    return *obj_;
+
   }
 
   template <typename T, class C>
   inline unique<T, C>::operator const data_type&() const
   {
-    // FIXME: Some code was deleted here (Check Typecasts overload).
+    
+    return get();
+
   }
 
   template <typename T, class C>
