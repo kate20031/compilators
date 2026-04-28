239e540d6e04fdbe2c204085af831cc3bd9ad092
kate20031 <83357098+kate20031@users.noreply.github.com>
2026-04-28 23:30:59 +0000

    TC-1/2: complete lexer and partial parser (AST factory)

--- src/parse/tiger-factory.hxx
+++ src/parse/tiger-factory.hxx
@@ -11,20 +11,20 @@ namespace parse
   inline ast::StringExp* make_StringExp(const location& location,
                                         std::string string)
   {
-    // FIXME: Some code was deleted here (Constructor of StringExp).
+    return new ast::StringExp(location, string);
   }
 
   inline ast::ObjectExp* make_ObjectExp(const location& location,
                                         ast::NameTy* type_name)
   {
-    // FIXME: Some code was deleted here (Constructor of Object).
+    return new ast::ObjectExp(location, type_name);
   }
 
   inline ast::CallExp* make_CallExp(const location& location,
                                     misc::symbol name,
                                     ast::exps_type* args)
   {
-    // FIXME: Some code was deleted here (Constructor of CallExp).
+    return new ast::CallExp(location, name, args);
   }
 
   inline ast::MethodCallExp* make_MethodCallExp(const location& location,
@@ -32,14 +32,14 @@ namespace parse
                                                 ast::exps_type* args,
                                                 ast::Var* object)
   {
-    // FIXME: Some code was deleted here (Constructor of MethodCallExp).
+return new ast::MethodCallExp(location, name, args, object);
   }
 
   inline ast::RecordExp* make_RecordExp(const location& location,
                                         ast::NameTy* type_name,
                                         ast::fieldinits_type* fields)
   {
-    // FIXME: Some code was deleted here (Constructor of RecordExp).
+    return new ast::RecordExp(location, type_name, fields);
   }
 
   inline ast::ArrayExp* make_ArrayExp(const location& location,
@@ -47,7 +47,7 @@ namespace parse
                                       ast::Exp* size,
                                       ast::Exp* init)
   {
-    // FIXME: Some code was deleted here (Constructor of ArrayExp).
+    return new ast::ArrayExp(location, type_name, size, init);
   }
 
   inline ast::NilExp* make_NilExp(const location& location)
@@ -58,13 +58,13 @@ namespace parse
   inline ast::SeqExp* make_SeqExp(const location& location,
                                   ast::exps_type* exps)
   {
-    // FIXME: Some code was deleted here (Constructor of SeqExp).
+    return new ast::SeqExp(location, exps);
   }
 
   inline ast::AssignExp*
   make_AssignExp(const location& location, ast::Var* var, ast::Exp* exp)
   {
-    // FIXME: Some code was deleted here (Constructor of AssignExp).
+    return new ast::AssignExp(location, var, exp);
   }
 
   inline ast::IfExp* make_IfExp(const location& location,
@@ -72,13 +72,13 @@ namespace parse
                                 ast::Exp* thenclause,
                                 ast::Exp* elseclause)
   {
-    // FIXME: Some code was deleted here (Constructor of IfExp).
+    return new ast::IfExp(location, test, thenclause);
   }
 
   inline ast::IfExp*
   make_IfExp(const location& location, ast::Exp* test, ast::Exp* thenclause)
   {
-    // FIXME: Some code was deleted here (Constructor of IfExp).
+    return new ast::IfExp(location, test, thenclause, nullptr);
   }
 
   inline ast::WhileExp*
@@ -97,13 +97,13 @@ namespace parse
 
   inline ast::BreakExp* make_BreakExp(const location& location)
   {
-    // FIXME: Some code was deleted here (Constructor of BreakExp).
+    return new ast::BreakExp(location);
   }
 
   inline ast::LetExp*
   make_LetExp(const location& location, ast::ChunkList* decs, ast::Exp* body)
   {
-    // FIXME: Some code was deleted here (Constructor of LetExp).
+    return new ast::LetExp(location, decs, body);
   }
 
   inline ast::OpExp* make_OpExp(const location& location,
@@ -129,7 +129,7 @@ namespace parse
   inline ast::FieldVar*
   make_FieldVar(const location& location, ast::Var* var, misc::symbol name)
   {
-    // FIXME: Some code was deleted here (Constructor of FieldVar).
+    return new ast::FieldVar(location, var, name);
   }
 
   inline ast::SubscriptVar*
@@ -138,7 +138,6 @@ namespace parse
     return new ast::SubscriptVar(location, var, index);
   }
 
-  /* Use expansion parameter pack to handle one or empty arguments */
   template <class... T> inline ast::exps_type* make_exps_type(T... exps)
   {
     return new ast::exps_type{exps...};
@@ -163,7 +162,7 @@ namespace parse
   inline ast::RecordTy* make_RecordTy(const location& location,
                                       ast::fields_type* fields)
   {
-    // FIXME: Some code was deleted here (Constructor of RecordTy).
+    return new ast::RecordTy(location, fields);
   }
 
   inline ast::ArrayTy* make_ArrayTy(const location& location,
@@ -248,4 +247,4 @@ namespace parse
   {
     return new ast::FunctionChunk(args...);
   }
-} // namespace parse
+}
